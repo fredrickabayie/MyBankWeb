1878e5733e02a54d77a52daa7cb07e6ab701e4aa
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-12-03 05:07:41 +0000

    Almost done with the implementation
    Device detection os next requirement

--- controllers/mybank.js
+++ controllers/mybank.js
@@ -24,7 +24,7 @@ function sendRequest ( url ) {
 
 $( function () {
    $("#addLocation_btn").click(function () {
-       alert("Add clicked");
+
        var longitude, latitude, bank, area, type, url, response;
 
        longitude = encodeURI(document.getElementById("longitudeVal").value);
@@ -34,17 +34,18 @@ $( function () {
        type = encodeURI(document.getElementById("selected-type").textContent);
 
        url = "http://cs.ashesi.edu.gh/~csashesi/class2016/fredrick-abayie/mobileweb/mybank/php/mybank.php" +
-           "?cmd=add_atmLocation&bankName="+bank+"&longitude="+longitude+"&latitude="+latitude+"&areaName="+area+"" +
+           "?cmd=add_location&bankName="+bank+"&longitude="+longitude+"&latitude="+latitude+"&areaName="+area+"" +
            "&type="+type;
 
        console.log(url);
 
        response = sendRequest(url);
        if (response.result === 1) {
-           alert(""+response.status);
-           document.getElementById("longitudeVal").innerHTML = "";
+           //alert(""+response.status);
+           document.getElementById("notification").textContent = response.status;
        } else {
-           alert(""+response.status);
+           //alert(""+response.status);
+           document.getElementById("notification").textContent = response.status;
        }
    });
 });
@@ -59,9 +60,49 @@ $('#type-menu').find('li a').click(function() {
     document.getElementById('selected-type').textContent = $(this).text();
 });
 
+
 $(function() {
-   //$('#viewloc').hide();
+   $('#viewLocations').hide();
+
+    var url, response, index, li = "";
+    url = "http://cs.ashesi.edu.gh/~csashesi/class2016/fredrick-abayie/mobileweb/mybank/php/mybank.php" +
+        "?cmd=rss_feed";
+
+    response = sendRequest(url);
+    if (response.result === 1) {
+
+        for (index = 1; index < 6; index++) {
+            li += '<li class="list-group-item list-group-item-success">';
+            li += '<a href='+response.rss[index].link+' accesskey='+index+'>' +
+                '<i style="color: #FF6600" class="fa fa-rss"></i></a>&nbsp;&nbsp;';
+            li += response.rss[index].title+'</li>';
+        }
+        $("#rss-feed").html(li);
+
+    } else {
+        alert(""+response.status);
+    }
 });
+
+
+$(function() {
+   $('#viewLocations-nav').click(function() {
+      $('#addLocations').hide();
+       $('#viewLocations').show();
+
+
+   });
+});
+
+
+$(function() {
+    $('#addLocations-nav').click(function() {
+        $('#addLocations').show();
+        $('#viewLocations').hide();
+    });
+});
+
+
 
 
 var geocoder = new google.maps.Geocoder();
@@ -97,9 +138,9 @@ function updateMarkerAddress(str) {
     document.getElementById('areaName').value = str;
 }
 
-function initialize() {
+function addInitialize() {
     var latLng = new google.maps.LatLng(5.759625, -0.219465);
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('addMap'), {
         zoom: 11,
         center: latLng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -126,10 +167,17 @@ function initialize() {
     });
 
     google.maps.event.addListener(marker, 'dragend', function() {
-        updateMarkerStatus('Drag ended');
+        updateMarkerStatus('Searched');
         geocodePosition(marker.getPosition());
     });
 }
 
 // Onload handler to fire off the app.
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', addInitialize);
+
+
+
+
+
+
+
